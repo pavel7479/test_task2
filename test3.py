ab623f3276a18c9f45ad7c384f1697f2c38fad5d
ipavel7479 <ipavel7479@gmail.com>
2024-07-21 16:37:10 +0300

    Сделали до теста1 включительно

--- test3.py
+++ test3.py
@@ -1,10 +1,18 @@
 import requests
-def get_students_with_param_requests(course: int):
-    url = "http://127.0.0.1:8000/students"
-    response = requests.get(url, params={"course": course})
-    return response.json()
 
+def get_student_by_id(student_id: int):
+    url = f"http://127.0.0.1:8000/students/id/{student_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.json())
+        return None
 
-students = get_students_with_param_requests(course=2)
-for student in students:
+student = get_student_by_id(student_id=5)
+if student:
     print(student)
+else:
+    print("Student not found")
+
+
